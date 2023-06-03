a68f46bb777ac215788ee11d67b9333157e42f1e
Sepehr Mohseni <sepmoh.97@gmail.com>
2023-06-03 23:31:44 +0330

    Update Request.php

--- framework/Http/Request.php
+++ framework/Http/Request.php
@@ -10,9 +10,7 @@ class Request
         public readonly array $cookies,
         public readonly array $files,
         public readonly array $server
-    )
-    {
-        
+    ) {
     }
 
     public static function globalsPack(): static
@@ -31,7 +29,7 @@ class Request
         return strtok($this->server['REQUEST_URI'], '?');
     }
 
-    public function getMethod():string
+    public function getMethod(): string
     {
         return $this->server['REQUEST_METHOD'];
     }
@@ -39,5 +37,34 @@ class Request
     public function getPostData(): array
     {
         return $this->postParams;
+    }
+
+    public function getHeader(string $name): ?string
+    {
+        $name = 'HTTP_' . str_replace('-', '_', strtoupper($name));
+        return $this->server[$name] ?? null;
+    }
+
+    public function isSecure(): bool
+    {
+        return !empty($this->server['HTTPS']) && $this->server['HTTPS'] !== 'off';
+    }
+
+    public function getClientIp(): string
+    {
+        if (!empty($this->server['HTTP_CLIENT_IP'])) {
+            $ip = $this->server['HTTP_CLIENT_IP'];
+        } elseif (!empty($this->server['HTTP_X_FORWARDED_FOR'])) {
+            $ip = $this->server['HTTP_X_FORWARDED_FOR'];
+        } else {
+            $ip = $this->server['REMOTE_ADDR'] ?? '0.0.0.0';
+        }
+
+        return filter_var($ip, FILTER_VALIDATE_IP) ?: '0.0.0.0';
+    }
+
+    public function getUserAgent(): string
+    {
+        return $this->server['HTTP_USER_AGENT'] ?? '';
     }
 }
